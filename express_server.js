434f5942faaf818ea6826a0becffeef3a8c4c86e
Nicholas Kotsos <nick.kotsos@gmail.com>
2021-04-16 04:02:22 +0000

    added about route

--- express_server.js
+++ express_server.js
@@ -6,11 +6,15 @@ const urlDatabase = {
   "b2xVn2": "http://www.happy.com",
   "9sm5xK": "http://www.google.com"
 };
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 app.get("/", (req, res) => {
   res.send("Hey hot stuff, want a date?");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/about", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
